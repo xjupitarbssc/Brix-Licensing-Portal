b8cc8a8714fb5623dd0aef9415aea8adc5457e07
xjupitarbssc <157065246+xjupitarbssc@users.noreply.github.com>
2024-02-15 12:28:45 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function openTab(tabName) {
+    var i, tabContent;
+
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    document.getElementById(tabName).style.display = "block";
+}
