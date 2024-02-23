3c71269d148c8115919c4b3e938569735909d9c5
xjupitarbssc <157065246+xjupitarbssc@users.noreply.github.com>
2024-02-23 08:53:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    openTab('bsd'); // Set the default tab
-});
-
-function openTab(tabName) {
-    var i, tabContent;
-
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
+function confirmDownload(productName) {
+    // Display the first popup with a notice and an OK button
+    if (confirm('Are you sure you want to download ' + productName + '? Click OK to proceed.')) {
+        // If OK is clicked, display the Terms of Service popup
+        showTermsOfService(productName);
     }
+}
 
-    document.getElementById(tabName).style.display = "block";
+function showTermsOfService(productName) {
+    // Display the Terms of Service popup
+    var agree = confirm('Please read and agree to the Terms of Service before downloading ' + productName +
+        '.\n\nI have read and agree to the terms of service.');
+
+    if (agree) {
+        // If the user agrees, initiate the download
+        initiateDownload(productName);
+    } else {
+        // If the user does not agree, display a message
+        alert('You must agree to the Terms of Service to proceed with the download.');
+    }
+}
+
+function initiateDownload(productName) {
+    // Simulate the download process or redirect to the actual download link
+    alert('Downloading ' + productName + '...');
+}
+
+function unavailableDownload() {
+    // Display a message for unavailable downloads
+    alert('This product is currently unavailable for download.');
 }
