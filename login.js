c7526379152ad13e69f83f45be18fd17d951dff8
xjupitarbssc <157065246+xjupitarbssc@users.noreply.github.com>
2024-03-03 13:22:36 -0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,5 @@
+function loginWithDiscord() {
+    // Simulate redirect to Discord authorization URL
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1213959196903477389&scope=identify&response_type=code&redirect_uri=https://brix.bloxstoreroblox.org/callback';
+    // Note: Replace YOUR_CLIENT_ID and YOUR_REDIRECT_URI with your actual Discord application details
+}
